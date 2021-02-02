e482dff1f16f21173fb6373325ce4c283322804e
Mariosor12 <marioavena120898@gmail.com>
2021-02-02 17:17:45 -0400

    Initial commit

---
+++ wp-config.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'bitnami_wordpress' );
+
+/** MySQL database username */
+define( 'DB_USER', 'bn_wordpress' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'f52c2ce3fd' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost:3306' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         '6b20fd1818268df6c9d873a69cb7025c2763b69c73938f12b92d7938f9f4f2f4');
+define('SECURE_AUTH_KEY',  '1a1e37a12d8e9196ec2c092eaf8da5194a9664771e2367f6f9857fb9d0168ded');
+define('LOGGED_IN_KEY',    '2f7ab1b2d12e03282185b0c314e138d4986d0473f307050555c7221a58da4d94');
+define('NONCE_KEY',        'a4543f21ff5b406ed5f2d51aa727094ce91428fad83eb94d1e4dc6c779e381ac');
+define('AUTH_SALT',        '50a0f0e137fe04e1d82ccf01d73b40cbd75852273e0f3af3ac315f4cc4e4fe78');
+define('SECURE_AUTH_SALT', 'b8e0d9cd39ef555d6448d551299b306d1dc1828b8a1702fb2ffa139f707822e4');
+define('LOGGED_IN_SALT',   '6e01da605f46b26f868baafe585a9df8e85f97fc1a364cd65d52cb34627b9dde');
+define('NONCE_SALT',       'c8f428e91adad28cf054d846b6b74203e682ea41abc281711cb179e52b735630');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* That's all, stop editing! Happy publishing. */
+/**
+ * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
+ * If you want to access only from an specific domain, you can modify them. For example:
+ *  define('WP_HOME','http://example.com');
+ *  define('WP_SITEURL','http://example.com');
+ *
+*/
+
+if ( defined( 'WP_CLI' ) ) {
+    $_SERVER['HTTP_HOST'] = 'localhost';
+}
+
+define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress');
+define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/wordpress');
+
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
+
+define('WP_TEMP_DIR', 'C:/xampp/apps/wordpress/tmp');
+
+
+//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
+//  More info at: https://docs.bitnami.com/general/apps/wordpress/troubleshooting/xmlrpc-and-pingback/
+
+if ( !defined( 'WP_CLI' ) ) {
+    // remove x-pingback HTTP header
+    add_filter('wp_headers', function($headers) {
+        unset($headers['X-Pingback']);
+        return $headers;
+    });
+    // disable pingbacks
+    add_filter( 'xmlrpc_methods', function( $methods ) {
+            unset( $methods['pingback.ping'] );
+            return $methods;
+    });
+    add_filter( 'auto_update_translation', '__return_false' );
+}
---
+++ wp-content/themes/lz-food-recipee/readme.txt
@@ -0,0 +1,90 @@
+=== LZ Food Recipee ===
+Contributors: Luzuk
+Tags: custom-background, custom-menu, custom-colors, custom-logo, editor-style, featured-images, full-width-template, footer-widgets, one-column, sticky-post, two-columns, theme-options, threaded-comments, right-sidebar, left-sidebar, grid-layout, three-columns, blog, e-commerce
+Requires at least: 5.0
+Tested up to: 5.5.3
+Requires PHP: 5.2.4
+Stable tag: 0.2.4
+License: GNU General Public License v3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+LZ Food Recipee is a child theme of lzrestaurant, It is captivating and elegant WordPress restaurant theme developed especially for websites that deal with restaurants, coffee shops, bistro, recipe,chefs, cafes, cakes and bakery parlour, places serving different cuisines, fast food centres, Chinese dishes, and other eatery businesses. 
+
+== Description ==
+
+LZ Food Recipee is a child theme of lzrestaurant, It is captivating and elegant WordPress restaurant theme developed especially for websites that deal with restaurants, coffee shops, bistro, recipe,chefs, cafes, cakes and bakery parlour, places serving different cuisines, fast food centres, Chinese dishes, and other eatery businesses. This theme serves everyone from food critics to food bloggers. Establish a beautiful website for bakery, barbecue, hotel, food joint, Italian restaurant, lodge, and grill house with absolute ease. This multipurpose restaurant WP theme is mobile responsive supporting all screen sizes. It is so user-friendly and easily customizable that you don’t have to be a professional developer to work on it. It offers ample of personalization options to modify the theme into your choice of look and feel. Shortcodes will help you include different functionality without indulging in the source code. The social media icons will give it the required exposure. The SEO factor will guarantee a good rank of your site in the search engine results. Launch your very own fully functional WordPress restaurant website with this amazing theme now!
+
+== Resources ==
+
+LZ Food Recipee WordPress Theme, Copyright 2018 Luzuk
+LZ Food Recipee is distributed under the terms of the GNU GPL
+
+LZ Food Recipee is a child theme of LZ Restaurant and as such uses the same opensource projects as its parent theme.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+LZ Food Recipee bundles the following third-party resources:
+
+*	PxHere Images, 
+	License: CC0 1.0 Universal (CC0 1.0)
+	Source: https://pxhere.com/en/license
+
+	Slider image
+	License: CC0 1.0 Universal (CC0 1.0)
+	Source: https://pxhere.com/en/photo/610407
+
+	Restaurant Menu image
+	License: CC0 1.0 Universal (CC0 1.0)
+	Source: https://pxhere.com/en/photo/1193806
+
+	Restaurant Menu image
+	License: CC0 1.0 Universal (CC0 1.0)
+	Source: https://pxhere.com/en/photo/636324
+
+	Restaurant Menu image
+	License: CC0 1.0 Universal (CC0 1.0)
+	Source: https://pxhere.com/en/photo/774753
+
+	Restaurant Menu image
+	License: CC0 1.0 Universal (CC0 1.0)
+	Source: https://pxhere.com/en/photo/492260
+
+== Changelog ==
+
+= 0.1 =
+	- Initial Version Released
+	
+= 0.2 =
+	- Changed Navigation style.
+	- Changed some theme code theme.
+
+= 0.2.1 =
+	- Resolved the declare class LZrestaurant_Customize issue.
+
+= 0.2.2 =
+	- Added Keyboard navigation.
+	- Added skip to content.
+	- Changed headings as per accessability.
+	- Resolved some theme errors.
+
+= 0.2.3 =
+	- done prefixing of global variables.
+	- Added "Tested up to" & "Requires PHP" in style.css file.
+	- Removed link tag from header.php file.
+	- Removed "@package WordPress" from all files.
+	- Removed esc_html from echoing function.
+	- Changed demo & pro urls.
+	- Changed screenshot & its image urls.
+
+= 0.2.4 =
+	- Changed some toggle button code.
+	- Added Stable tag in readme.txt file.
+	- added one codition in header.php file.
